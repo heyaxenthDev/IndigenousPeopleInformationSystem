a9225095c61e85c1b1c7f07a869f829637af6168
heyaxenthDev <hyacynth.dev@gmail.com>
2025-05-26 00:57:03 +0800

    database update

--- database/ip_laua_an_db.sql
+++ database/ip_laua_an_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2025 at 12:27 PM
+-- Generation Time: May 25, 2025 at 06:56 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
